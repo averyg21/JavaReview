3645c32c7ba27b1893e8b3fd3fb23e548a413a2b
Avery Grimes <32044372+averyg21@users.noreply.github.com>
2017-12-01 08:49:12 -0600

    Add files via upload

---
+++ Recursion/src/recursion/Recursion.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recursion;
+
+/**
+ *
+ * @author Avery
+ */
+public class Recursion 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Recursion recur = new Recursion();
+       String s = "ZERFOREZ";
+       String f = "helfpe";
+       recur.isPal(s);
+//        recur.isPal(f);
+        
+//        recur.fact(4);
+        
+        //recur.fibonacci(5);
+        
+        
+    }
+    
+    public boolean isPal(String s)
+    {
+        if (s.length() == 0 || s.length() == 1)
+        {
+            System.out.println("\nIs a Palindrome.");
+            return true;
+        }
+        if(s.charAt(0) == s.charAt(s.length() - 1))
+        {
+            System.out.println("Still searching.");
+            return isPal(s.substring(1, s.length() - 1));
+        }
+        
+        System.out.println("\nNot a Palindrome.");
+        return false;
+    }
+    
+    public int fact (int n)
+    {
+        int result;
+        
+        if (n == 1)
+        {
+            return 1;
+        }
+        
+        result = fact(n-1) * n;
+        System.out.println(result);
+        return result;
+    }
+    
+    public int fibonacci( int n)
+    {
+        int f;
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if ( n == 1)
+        {
+            return 1;
+        }
+        else 
+        {
+            
+            f = fibonacci (n - 1) + fibonacci (n - 2);
+            System.out.println(f);
+            return f;
+        }
+                
+    }
+    
+}
